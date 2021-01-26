5aeaf0f41caf1eab535de4c11d5bd2be0e4d23ed
Luis Canchari <luis.andres.canchari@gmail.com>
2021-01-25 22:04:02 -0500

    Carga inicial

---
+++ AppDemo01/src/main/java/edu/cientifica/appdemo/AppDemo01Application.java
@@ -0,0 +1,13 @@
+package edu.cientifica.appdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDemo01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDemo01Application.class, args);
+	}
+
+}
---
+++ AppDemo01/src/test/java/edu/cientifica/appdemo/AppDemo01ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cientifica.appdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppDemo01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
